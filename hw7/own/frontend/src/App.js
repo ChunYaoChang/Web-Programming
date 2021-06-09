3767b7ef78759b79f23b008a301415992d4932da
ChunYaoChang <b07902022@csie.ntu.edu.tw>
2021-06-09 22:55:51 +0800

    tmp save

---
+++ hw7/own/frontend/src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import SignIn from "./containers/SignIn";
+import ChatRoom from "./containers/ChatRoom"
+import {message} from "antd"
+const LOCALSTORAGE_KEY = "save-me";
+
+const App = () => {
+  const savedMe = localStorage.getItem(LOCALSTORAGE_KEY);
+  const [signedIn, setSignedIn] = useState(false);
+  const [me, setMe] = useState(savedMe || "");
+  useEffect(() => {
+    if (signedIn) {
+      localStorage.setItem(LOCALSTORAGE_KEY, me);
+    }
+  }, [signedIn]);
+
+  const displayStatus = (payload) => {
+    if (payload.msg) {
+      const { type, msg } = payload
+      const content = {
+        content: msg, duration: 0.5 }
+      switch (type) {
+        case 'success':
+          message.success(content)
+          break
+        case 'error':
+        default:
+          message.error(content)
+          break
+  }}}
+  // useEffect(() => {
+  //   displayStatus(status)}, [status])
+
+  return (
+    <div className="App">
+      {signedIn ? 
+        (<ChatRoom me={me} displayStatus={displayStatus}/>) : 
+        (<SignIn me={me} setMe={setMe} setSignedIn={setSignedIn} displayStatus={displayStatus}/>)}
+    </div>
+  );
+};
+export default App;
